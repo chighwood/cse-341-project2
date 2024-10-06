84ef5ba678bdcf108e44a4073d3ea662534a3707
chighwood <hig23003@byui.edu>
2024-10-05 21:19:17 -0500

    Deleted Files for Debugging

--- controllers/cars.js
+++ controllers/cars.js
@@ -42,36 +42,6 @@ const createCar = async (req, res) => {
     }
 };
 
-// const updateCar = async (req, res) => {
-//     try {
-//         console.log('Request Body:', req.body);
-//         const carId = new ObjectId(req.params.id);
-
-//         const car = {
-//             brand: req.body.brand,
-//             name: req.body.name,
-//             year: req.body.year,
-//             bodyStyle: req.body.bodyStyle,
-//             color: req.body.color
-//         };
-
-//         if (!car.brand && !car.name && !car.year && !car.bodyStyle && !car.color) {
-//             return res.status(400).json({ message: 'No valid fields to update.' });
-//         }
-
-//         const response = await mongodb.getDatabase().db().collection('cars').replaceOne({ _id: carId }, car);
-
-//         if (response.modifiedCount > 0) {
-//             res.status(200).send('Car updated successfully');
-//         } else {
-//             res.status(404).json({ message: 'Car not found or no changes made.' });
-//         }
-//     } catch (error) {
-//         console.error('Error occurred during car update:', error.message);
-//         res.status(400).json({ message: 'Invalid ID format or update error.' });
-//     }
-// };
-
 const updateCar = async (req, res) => {
     const carId = new ObjectId(req.params.id);
     const car = {
